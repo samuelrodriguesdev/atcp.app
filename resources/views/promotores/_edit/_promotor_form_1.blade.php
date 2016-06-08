c6b573cc8d603977b5caea49bfe39a1e7d5539da
rubensilva24 <rubensilvafutsal@gmail.com>
2016-06-08 16:22:29 +0100

    wwwwwwwwwwee

--- resources/views/promotores/_edit/_promotor_form_1.blade.php
+++ resources/views/promotores/_edit/_promotor_form_1.blade.php
@@ -3,55 +3,55 @@
         @include('promotores._edit._promotor_form_1_1')
     </div> 
 </div>   
-<div class="box box-default">
+
+<div class="box box-default collapsed-box">
   <div class="box-header with-border">
     <h3 class="box-title">1. Identificação Pessoal</h3>
     <div class="box-tools pull-right">
-      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-  </div><!-- /.box-tools -->
-</div><!-- /.box-header -->
-<div class="box-body">
-    @include('promotores._edit._promotor_form_1_2')
-</div><!-- /.box-body -->
+      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+    </div><!-- /.box-tools -->
+  </div><!-- /.box-header -->
+  <div class="box-body">
+  @include('promotores._edit._promotor_form_1_2')
+  </div><!-- /.box-body -->
 </div><!-- /.box -->
 
-<div class="box box-default">
+<div class="box box-default collapsed-box">
   <div class="box-header with-border">
     <h3 class="box-title">2. Situação Face ao Emprego</h3>
     <div class="box-tools pull-right">
-      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-  </div><!-- /.box-tools -->
-</div><!-- /.box-header -->
-<div class="box-body">
-    @include('promotores._edit._promotor_form_1_3')
-</div><!-- /.box-body -->
+      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+    </div><!-- /.box-tools -->
+  </div><!-- /.box-header -->
+  <div class="box-body">
+@include('promotores._edit._promotor_form_1_3')
+  </div><!-- /.box-body -->
 </div><!-- /.box -->
 
-<div class="box box-default">
+
+<div class="box box-default collapsed-box">
   <div class="box-header with-border">
     <h3 class="box-title">3. Empreendedorismo de Negócios</h3>
     <div class="box-tools pull-right">
-      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-  </div><!-- /.box-tools -->
-</div><!-- /.box-header -->
-<div class="box-body">
+      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+    </div><!-- /.box-tools -->
+  </div><!-- /.box-header -->
+  <div class="box-body">
     @include('promotores._edit._promotor_form_1_4')
-</div>
-</div><!-- /.box-body -->
-<!-- /.box -->
+  </div><!-- /.box-body -->
+</div><!-- /.box -->
 
-<div class="box box-default">
+<div class="box box-default collapsed-box">
   <div class="box-header with-border">
     <h3 class="box-title">4. Caracterização do Projecto/Negócio</h3>
     <div class="box-tools pull-right">
-      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-  </div><!-- /.box-tools -->
-</div><!-- /.box-header -->
-<div class="box-body">
-    @include('promotores._edit._promotor_form_1_4')
-</div>
-</div><!-- /.box-body -->
-<!-- /.box -->    
+      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+    </div><!-- /.box-tools -->
+  </div><!-- /.box-header -->
+  <div class="box-body">
+    @include('promotores._edit._promotor_form_1_5')
+  </div><!-- /.box-body -->
+</div><!-- /.box -->   
 
 <div class="box-footer">
     <input class="btn bg-green btn-flat pull-right" type="submit" value="Guardar" />
