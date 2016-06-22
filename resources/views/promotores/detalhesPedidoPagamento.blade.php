cd46c8a9d5bc7c121912e7032ced48b46ffd62c9
samuelrodriguesdev <samuelrodrigues.dev@gmail.com>
2016-06-22 17:56:02 +0100

    dasdasd
    da

--- resources/views/promotores/detalhesPedidoPagamento.blade.php
+++ resources/views/promotores/detalhesPedidoPagamento.blade.php
@@ -13,13 +13,13 @@
                 <label for="data_pedido_pagamento">Data P.P</label>
                 <input type="text" name="data_pedido_pagamento" class="form-control date" value="{{ $pp->data_pedido_pagamento }}" required>
             </div>
-             <div class="form-group has-feedback col-md-6">
+            <div class="form-group has-feedback col-md-6">
                 <label for="data_recebimento_pagamento">Data Recebimento P.P</label>
                 <input type="text" name="data_recebimento_pagamento" class="form-control date" value="{{ $pp->data_recebimento_pagamento }}" required>
             </div>
         </div>
-         <div class="row">
-             <div class="form-group has-feedback col-md-5">
+        <div class="row">
+            <div class="form-group has-feedback col-md-5">
                 <label for="valor_pedido_pagamento">Valor P.P</label>
                 <input type="text" name="valor_pedido_pagamento" id="valor_pedido_pagamento" class="form-control" value="{{ $pp->valor_pedido_pagamento }}" required>
             </div>
@@ -33,9 +33,15 @@
                     <option value="3" {{ $pp->estado_pedido_pagamento == 3 ? 'selected' : '' }} >Liquidado Parcialmente</option>
                 </select>
             </div>
-            <div class="form-group has-feedback col-md-5" id="toggle_valor" {{ $pp->estado_pedido_pagamento == 3 ? '' : 'style=display:none;' }} >
+        </div>
+        <div class="row">
+            <div class="form-group has-feedback col-md-6" id="toggle_data" {{ $pp->estado_pedido_pagamento == 3 || 2 ? '' : 'style=display:none;' }}>
+                <label for="data_recebimento_pagamento">Data Recebimento P.P</label>
+                <input type="text" name="data_recebimento_pagamento" class="form-control date" value="{{ $pp->data_recebimento_pagamento }}">
+            </div>
+            <div class="form-group has-feedback col-md-6" id="toggle_valor" {{ $pp->estado_pedido_pagamento == 3 ? '' : 'style=display:none;' }}>
                 <label for="valor_pago_pagamento">Valor Recebido P.P</label>
-                <input type="text" name="valor_pago_pagamento" id="valor_pago_pagamento" class="form-control" value="{{ $pp->valor_pago_pagamento }}">
+                <input type="text" name="valor_pago_pagamento" id="valor_pago_pagamento" value="{{ $pp->valor_pago_pagamento }}" class="form-control">
             </div>
         </div>
         <label for="observacoes">Observações</label>
@@ -64,8 +70,12 @@
 
     $( '#estado_pedido_pagamento' ).select2().on('select2:select', function (){
         $('#toggle_valor').hide();
-        if (this.value ==3) {
+        $('#toggle_data').hide();
+        if (this.value ==2) {
+            $('#toggle_data').show();
+        } else if (this.value ==3) {
             $('#toggle_valor').show();
+            $('#toggle_data').show();
         }
     });
 
